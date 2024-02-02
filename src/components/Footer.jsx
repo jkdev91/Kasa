391d6b31e6237753da8f6d5b8159b9cc072ed737
dev91junior <devjk91@outlook.com>
2024-02-02 14:36:05 +0100

    create & style Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+
+const Footer = () => {
+
+    return (
+        <>
+            <footer>
+            
+                <img src='../logo/LOGO-footer.png' alt="logo kasa footer" />
+                <p>© 2020 Kasa. All rights reserved</p>
+    
+
+              
+            </footer>
+
+           
+        </>
+    )
+  }
+  
+  export default Footer
